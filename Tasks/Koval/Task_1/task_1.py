fa6c9a7248555da55455432b3c808b73b5dc282c
Kovalik <Ldidi1@yandex.ru>
2022-01-14 22:51:18 +0300

    Create task_1.py

---
+++ Tasks/Koval/Task_1/task_1.py
@@ -0,0 +1,23 @@
+# Депозит:
+# начальная сумма - 20000 BYN
+# срок - 5 лет 
+# процент (годовой) - 15%
+#! ежемесячная капитализация!
+
+
+# Вычислить сумму на счету в конце указанного срока.
+import decimal
+
+deposit = 20000
+term = 5
+percent = 0.15
+
+# counting months
+term_in_month = term * 12
+
+# Thx Internet for this magic
+# intermediate floating point number calculation
+k = decimal.Decimal(percent) / decimal.Decimal(12)
+
+result = deposit * (1 + k)**term_in_month
+print (round(result,2))
