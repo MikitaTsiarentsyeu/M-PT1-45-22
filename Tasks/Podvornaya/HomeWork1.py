2652c94cc5f03f1fcaf0ef38b7a505816288135e
marta-podvornaya <marta.podvornaya@gmail.com>
2022-01-19 21:01:24 +0300

    Update HomeWork1.py

--- Tasks/Podvornaya/HomeWork1.py
+++ Tasks/Podvornaya/HomeWork1.py
@@ -1,3 +1,4 @@
+#first solution using while
 import decimal
 
 deposit = decimal.Decimal('20000')
@@ -9,4 +10,12 @@ while term <60:
     deposit = round(deposit, 2)
     term +=1
 
-print('Ваш депозит составляет: ', deposit)
+print('Your deposit is worth: ', deposit, 'BYN')
+
+#second solution using complex persent formula
+
+profit = 1 + 15 / (12*100) ** 60 #mounthly capitalization interest
+profit = decimal.Decimal(profit)
+deposit = deposit * profit
+deposit = round(deposit, 2)
+print('Your deposit is worth: ', deposit, 'BYN')
