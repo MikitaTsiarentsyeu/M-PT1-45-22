defa4295f6da1d9eff4726c1f01c7420a89c5421
Andrey1Kruk <andrey.kruk.cob555@bk.ru>
2022-01-20 20:12:08 +0300

    update_task_1

--- Tasks/Andrey Kruk/task1.py
+++
@@ -1,9 +0,0 @@
-from math import pow
-
-deposit_amount = 20000
-deposit_rate = 15
-deposit_period = 5
-
-
-deposit_result = deposit_amount * pow((1 + deposit_rate/(100*12)), (deposit_period*12))
-print(deposit_result)
