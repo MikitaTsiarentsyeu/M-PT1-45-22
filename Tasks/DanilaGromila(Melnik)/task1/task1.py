83e0987bfe0179afcda34f6694cfabf480483c74
DanilaGromila <d.gromila@yahoo.com>
2022-01-20 23:21:17 +0300

    task1
    DONE

--- Tasks/DanilaGromila(Melnik)/task1/task1.py
+++ Tasks/DanilaGromila(Melnik)/task1/task1.py
@@ -1,8 +1,10 @@
 import decimal
+import math
 Deposit = 20000
 term = 5 
 p = 0.15
 month = term*12
 bank=decimal.Decimal(p) / decimal.Decimal(12)
 S = Deposit * (1 + bank)**month
+round(decimal.Decimal(S))# а почему не округляет?
 print(S)
