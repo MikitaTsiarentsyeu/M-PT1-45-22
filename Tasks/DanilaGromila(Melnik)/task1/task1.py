effe0af70d8e127f1c04a7d098fa0c275e359178
DanilaGromila <d.gromila@yahoo.com>
2022-01-21 17:49:18 +0300

    task1
    done

--- Tasks/DanilaGromila(Melnik)/task1/task1.py
+++ Tasks/DanilaGromila(Melnik)/task1/task1.py
@@ -6,5 +6,5 @@ p = 0.15
 month = term*12
 bank=decimal.Decimal(p) / decimal.Decimal(12)
 S = Deposit * (1 + bank)**month
-round(decimal.Decimal(S))# а почему не округляет?
+round(decimal.Decimal(S))#  почему не округляет?
 print(S)
