8237685261775fdf01f52ad73062b8619948f41f
MakRag10 <97477748+MakRag10@users.noreply.github.com>
2022-01-21 23:06:16 +0300

    added  "round"

--- Tasks/Rabushenka/Tasks1/HomWr-1.py
+++ Tasks/Rabushenka/Tasks1/HomWr-1.py
@@ -8,4 +8,5 @@ print(f'investment period in days = {exec}')
 tran_on = rate / 100
 print(f'interest rate transfer = {tran_on}')
 Final_sum = Sum * (1 + tran_on/days)**exec
-print(f'account balance at the end of the term = {Final_sum}')
+Final_sum_M = round(Final_sum,2)
+print(f'account balance at the end of the term = {Final_sum_M}')
