0d20d64d03e4f605fede91f9b63dafdf878cae57
MakRag10 <97477748+MakRag10@users.noreply.github.com>
2022-01-21 15:29:07 +0300

    created a new folder and changed run. to eng.

---
+++ Tasks/Rabushenka/Tasks1/HomWr-1.py
@@ -0,0 +1,11 @@
+import math
+Sum = 20000
+year = 5
+rate = 15
+days = 365
+exec = days * year
+print(f'investment period in days = {exec}')
+tran_on = rate / 100
+print(f'interest rate transfer = {tran_on}')
+Final_sum = Sum * (1 + tran_on/days)**exec
+print(f'account balance at the end of the term = {Final_sum}')
