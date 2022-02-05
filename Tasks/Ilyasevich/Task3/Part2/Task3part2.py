cd4f03c3b64e523fe59a35f43c6666c2d72b521b
ahmatchin <conduire@list.ru>
2022-02-05 19:34:15 +0300

    Update Task3part2.py

--- Tasks/Ilyasevich/Task3/Part2/Task3part2.py
+++ Tasks/Ilyasevich/Task3/Part2/Task3part2.py
@@ -15,8 +15,7 @@
 # 'the' встречается 6 раза
 # 'and' встречается 5 раза
 
-from operator import itemgetter, attrgetter, methodcaller
-enter_string = "Python Software Foundation (PSF) is a 501 non-profit corporation that holds the intellectual property rights behind the Python programming language. We manage the open source licensing for Python version 2.1 and later and own and protect the trademarks associated with Python. We also run the North American PyCon conference annually, support other Python conferences around the world, and fund Python related development with our grants program and by funding special projects".lower().split(' ')
+data = input('Input text: \n').lower().replace(",", " ").replace("."," ").replace("  ", " ").split(' ')
 enter_elements = input('Input the number of elements: \n')
 
 while True:
@@ -24,11 +23,18 @@ while True:
         break
     if enter_elements.isdigit() == False:
         enter_elements = input('Вы ввели букву)\n''Input the number of elements: \n')
+
 enter_elements = int(enter_elements)
-result = {i: enter_string.count(i) for i in enter_string}
+words_dict = {}
+for word in data:
+    words_dict[word] = data.count(word)
+print(words_dict)
+result = {val[0]: val[1] for val in sorted(words_dict.items(), key=lambda x: (-x[1], x[0]))}
+
 i = 1
 for key in sorted(result, key=result.get, reverse=True):
-    if i <= enter_elements and sorted(result.items(), key=lambda x: (x[1], x[0])):
+    if i <= enter_elements:
         print(f'{key} встречается {result[key]}')
     i += 1
+
 
