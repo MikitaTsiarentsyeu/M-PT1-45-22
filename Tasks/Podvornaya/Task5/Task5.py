7bc2334ec84818aa2933a26f283c3d2b1b001a2f
marta-podvornaya <marta.podvornaya@gmail.com>
2022-02-16 19:57:00 +0300

    Implement Task5

---
+++ Tasks/Podvornaya/Task5/Task5.py
@@ -0,0 +1,57 @@
+import math
+
+
+def check_str(string):
+    string = string.replace(" ", "")
+    string = [x for x in string]
+    uppers = 0
+    lowers = 0
+    for i in string:
+        if i.isupper():
+            uppers += 1
+        else:
+            lowers += 1
+    print(f"{uppers} upper case, {lowers} lower case")
+
+
+check_str('The quick Brow Fox')
+
+
+def is_prime(number):
+    i = 2
+    while i <= math.sqrt(number):
+        if number % i == 0:
+            return print("False")
+        i += 1
+    print("True")
+
+
+is_prime(787)
+is_prime(777)
+
+
+def get_ranges(l):
+    ranged = ""
+    l.append('')  # adding an element to be able to operate the last element of the list
+    i = 0
+    while i < len(l)-1:
+        intermediate = []
+        while l[i] + 1 == l[i+1]:
+            intermediate.append(l[i])
+            i += 1
+        else:
+            if l[i] - 1 == l[i-1]:
+                intermediate.append(l[i])
+            if len(intermediate):
+                ranged += f"{intermediate[0]}-{intermediate[-1]}, "
+                i += 1
+            else:
+                ranged += f"{l[i]}, "
+                i += 1
+    ranged = ranged[:-2]  # cutting off excess comma and space
+    return ranged
+
+
+print(get_ranges([0, 1, 2, 3, 4, 7, 8, 10]))
+print(get_ranges([4, 7, 10]))
+print(get_ranges([2, 3, 8, 9]))
