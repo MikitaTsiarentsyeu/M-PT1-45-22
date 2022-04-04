37cf9c48434e597e6f84777f8be6b3e2461ad388
Mikita Tsiarentsyeu <Mikita_Tsiarentsyeu@epam.com>
2022-04-05 00:23:52 +0600

    add lesson 05/04

--- Lessons/web project/proj/app/views.py
+++ Lessons/web project/proj/app/views.py
@@ -1,4 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post, Author
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {"test": 1234})
+
+# def posts(request):
+#     posts = Post.objects.all()
+#     post_list=''
+
+#     for post in posts:
+#         post_list += f"<li><h3>{post.title}</h3><p>by {post.author.name}</p></li>"
+
+#     resp = f"<h1>All posts:</h1><ul>{post_list}</ul>"
+#     return HttpResponse(resp)
+
+def posts(request):
+    posts = Post.objects.all()
+    return render(request, 'posts.html', {'posts':posts})
+
+# def post(request, post_id):
+#     p = Post.objects.get(id=post_id)
+#     resp = f"<h1>{p.title}</h1><p>{p.content}</p>"
+#     return HttpResponse(resp)
+
+def post(request, post_id):
+    p = Post.objects.get(id=post_id)
+    return render(request, 'post.html', {'post': p})
--- Lessons/web project/proj/proj/urls.py
+++ Lessons/web project/proj/proj/urls.py
@@ -13,11 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from unicodedata import name
 from django.contrib import admin
 from django.urls import path
 from app import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home)
+    path('', views.posts, name='posts'),
+    path('posts/', views.posts, name='posts'),
+    path('posts/<int:post_id>', views.post, name='post'),
+    path('posts/<str:post_id>', views.post, name='post'),    
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
